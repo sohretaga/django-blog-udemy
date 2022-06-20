0e0e45f9d4b662a81de8316b895b7de8d21e8ce1
sohretaga <shohretaga@gmail.com>
2022-06-21 02:23:20 +0400

    Created category model

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blogapp.models import CategoryModule
 
 # Register your models here.
+
+admin.site.register(CategoryModule)
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,17 @@
+from tabnanny import verbose
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
+
+class CategoryModule(models.Model):
+  name = models.CharField(max_length=30, blank=False, null=False)
+  slug = AutoSlugField(populate_from = 'name', unique=True)
+
+  class Meta:
+    db_table = 'category'
+    verbose_name_plural = 'Kateqoriyalar'
+    verbose_name = 'Kateqori'
+  
+  def __str__(self) -> str:
+    return self.name
