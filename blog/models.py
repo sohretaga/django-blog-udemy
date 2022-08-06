b7d46479c7f43db640d06ae77c1a8374836b2c0e
sohretaga <shohretaga@gmail.com>
2022-08-06 23:50:53 +0400

    Created category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
+
+class Category(models.Model):
+  title = models.CharField(max_length=50, blank=False, null=False)
+  slug = AutoSlugField(populate_from = 'title', unique = True)
+
+  class Meta:
+    db_table = 'category'
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
+
+  def __str__(self) -> str:
+    return self.title
