43875b89c818b98e1ad6e91fa6ec32739861b504
sohretaga <shohretaga@gmail.com>
2022-08-07 00:07:59 +0400

    Created post model

--- blog/admin.py
+++ blog/admin.py
@@ -6,3 +6,7 @@ from . import models
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ('title', 'slug')
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('title', 'category', 'author', 'created_date')
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from autoslug import AutoSlugField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,3 +15,19 @@ class Category(models.Model):
 
   def __str__(self) -> str:
     return self.title
+
+
+class Post(models.Model):
+  title = models.CharField(max_length=50)
+  content = models.TextField()
+  image = models.ImageField(upload_to = 'post_images')
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_post')
+  slug = AutoSlugField(populate_from = 'title', unique = True)
+  category = models.ManyToManyField(Category, related_name='category_post')
+  created_date = models.DateTimeField(auto_now_add=True)
+  update_date = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    db_table = 'post'
+    verbose_name = 'Post'
+    verbose_name_plural = 'Posts'
