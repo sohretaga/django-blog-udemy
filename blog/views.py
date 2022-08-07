5a91f16f9945ac9432578db0ac9407403cf82312
sohretaga <shohretaga@gmail.com>
2022-08-07 18:58:21 +0400

    Created blogs templayte and pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
   context = {
-    'blogs': Post.objects.all()
+    'blogs': Paginator(Post.objects.all().order_by('-created_date'), 2).get_page(request.GET.get('page'))
   }
   return render(request, 'pages/index.html', context)
 
