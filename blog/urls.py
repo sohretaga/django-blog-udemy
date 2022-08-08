0f9d9decd4fff71c2e3758694c7bf6c3f2857986
sohretaga <shohretaga@gmail.com>
2022-08-08 15:31:57 +0400

    Created categories and myblogs pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('contact/', views.contact, name='contact'),
+  path('category/<slug:slug>/', views.category, name='category'),
+  path('myblogs/', views.myblogs, name='myblogs')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -10,5 +10,19 @@ def index(request):
   }
   return render(request, 'pages/index.html', context)
 
+def category(request, slug):
+  context = {
+    'blogs': Paginator(get_object_or_404(Category, slug=slug).category_post.order_by('-created_date'), 2).get_page(request.GET.get('page')),
+    'category_name': Category.objects.get(slug=slug).title
+  }
+  return render(request, 'pages/categories.html', context)
+
 def contact(request):
   return render(request, 'pages/contact.html')
+
+
+def myblogs(request):
+  context = {
+    'blogs': Paginator(request.user.author_post.order_by('-created_date'), 2).get_page(request.GET.get('page'))
+  }
+  return render(request, 'pages/myblogs.html', context)
