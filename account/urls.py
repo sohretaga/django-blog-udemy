c783ad7c76ae6ae00ed2b1fb38436c62a51fb66c
sohretaga <shohretaga@gmail.com>
2022-08-13 16:58:27 +0400

    Created login form

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import userLogout, changePassword, updateAccount, signUp
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('logout/', userLogout, name='logout'),
   path('change-password/', changePassword, name='change-password'),
   path('update-account/', updateAccount, name='update-account'),
   path('sign-up/', signUp, name='sign-up'),
+  path('login/', auth_views.LoginView.as_view(
+    template_name = 'signin.html'
+    ), name='sign-in'),
+  
 ]
--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.CustomUser'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
